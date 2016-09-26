b28a6e91d463d7914e93405202438b34a943240f
Clemens Zeidler <clemens.zeidler@gmail.com>
2016-09-26 16:21:52 +1300

    Copyright.

--- src/main/java/org/fejoa/tests/gui/StorageDirViewer.java
+++ src/main/java/org/fejoa/tests/gui/StorageDirViewer.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2016.
+ * Distributed under the terms of the GPLv3 License.
+ *
+ * Authors:
+ *      Clemens Zeidler <czei002@aucklanduni.ac.nz>
+ */
 package org.fejoa.tests.gui;
 
 import javafx.application.Application;
