17321909d6b0878ca7e2a4fb487daa33fcba5044
Clemens Zeidler <clemens.zeidler@gmail.com>
2016-10-18 17:31:10 +1300

    Add a very basic contact branch checkout gui.

--- javafxgui/src/main/java/org/fejoa/gui/javafx/FileStorageView.java
+++ javafxgui/src/main/java/org/fejoa/gui/javafx/FileStorageView.java
@@ -29,6 +29,7 @@ import org.fejoa.library.database.DatabaseDiff;
 import org.fejoa.library.database.DefaultCommitSignature;
 import org.fejoa.library.database.IOStorageDir;
 import org.fejoa.library.database.StorageDir;
+import org.fejoa.library.remote.ChunkStorePullJob;
 import org.fejoa.library.support.Task;
 import org.json.JSONException;
 
@@ -75,6 +76,153 @@ class FileStorageEntry implements IStorageDirBundle {
     }
 }
 
+class ContactFileStorageView extends VBox {
+    static public class Entry extends TreeItem<String> {
+        final public BranchInfo.Location location;
+
+        public Entry(String name) {
+            super(name);
+
+            location = null;
+        }
+
+        public Entry(String name, BranchInfo.Location location) {
+            super(name);
+
+            this.location = location;
+        }
+    }
+
+    final private Client client;
+    final private UserData userData;
+    final private HBox optionLayout = new HBox();
+    final private TreeView<String> treeView = new TreeView<>();
+
+    final private StorageDir.IListener listener = new StorageDir.IListener() {
+        @Override
+        public void onTipChanged(DatabaseDiff diff, String base, String tip) {
+            update();
+        }
+    };
+
+    public ContactFileStorageView(Client client) {
+        this.client = client;
+        this.userData = client.getUserData();
+        userData.getStorageDir().addListener(listener);
+
+        final Entry item = new Entry("Contact File Storage");
+        item.setExpanded(true);
+        treeView.setRoot(item);
+
+        getChildren().add(optionLayout);
+        getChildren().add(treeView);
+
+        final Button checkoutButton = new Button("Check Out");
+        checkoutButton.setDisable(true);
+        optionLayout.getChildren().add(checkoutButton);
+        checkoutButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                try {
+                    checkOut();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (CryptoException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        treeView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TreeItem<String>>() {
+            @Override
+            public void changed(ObservableValue<? extends TreeItem<String>> observable, TreeItem<String> old,
+                                TreeItem<String> newItem) {
+                if (newItem == null) {
+                    checkoutButton.setDisable(true);
+                    return;
+                }
+                Entry entryItem = (Entry)newItem;
+                checkoutButton.setDisable(entryItem.location == null);
+            }
+        });
+        update();
+    }
+
+    private void checkOut() throws IOException, CryptoException {
+        Entry selectedEntry = (Entry)treeView.getSelectionModel().getSelectedItem();
+        final BranchInfo.Location location = selectedEntry.location;
+        client.pullContactBranch(location.getRemote(), location, new Task.IObserver<Void, ChunkStorePullJob.Result>() {
+            @Override
+            public void onProgress(Void aVoid) {
+
+            }
+
+            @Override
+            public void onResult(ChunkStorePullJob.Result result) {
+                try {
+                    String branchName = location.getBranchInfo().getBranch();
+                    File destination = new File(client.getContext().getHomeDir(), branchName);
+                    StorageDir indexStorage = client.getContext().getPlainStorage(new File(destination, ".index"),
+                            branchName);
+                    Index index = new Index(indexStorage);
+                    CheckoutDir checkoutDir = new CheckoutDir(userData.getStorageDir(location.getBranchInfo()), index, destination);
+                    checkoutDir.checkOut().start(new Task.IObserver<CheckoutDir.Update, CheckoutDir.Result>() {
+                        @Override
+                        public void onProgress(CheckoutDir.Update update) {
+
+                        }
+
+                        @Override
+                        public void onResult(CheckoutDir.Result result) {
+
+                        }
+
+                        @Override
+                        public void onException(Exception exception) {
+                            exception.printStackTrace();
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (CryptoException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onException(Exception exception) {
+                exception.printStackTrace();
+            }
+        });
+    }
+
+    private void update() {
+        treeView.getRoot().getChildren().clear();
+        for (ContactPublic contactPublic : userData.getContactStore().getContactList().getEntries()) {
+            Remote remote = contactPublic.getRemotes().getDefault();
+            String remoteString = remote.toAddress();
+            Entry item = new Entry(contactPublic.getId() + " (" + remoteString + ")");
+            int nContactBranches = 0;
+            try {
+                for (BranchInfo branchInfo : contactPublic.getBranchList().getEntries()) {
+                    nContactBranches++;
+                    Entry branchItem = new Entry(branchInfo.getBranch());
+                    item.getChildren().add(branchItem);
+                    for (BranchInfo.Location location : branchInfo.getLocations().getEntries()) {
+                        Entry locationItem = new Entry(location.getRemote().toAddress(), location);
+                        branchItem.getChildren().add(locationItem);
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if (nContactBranches > 0)
+                treeView.getRoot().getChildren().add(item);
+        }
+    }
+
+}
+
 public class FileStorageView extends VBox {
     final private Client client;
     final StatusManagerMessenger statusManager;
@@ -89,6 +237,7 @@ public class FileStorageView extends VBox {
     };
 
     final ListView<FileStorageEntry> fileStorageListView;
+    final ContactFileStorageView contactFileStorageListView;
 
     public FileStorageView(Client client, IStatusManager statusManager) {
         this.client = client;
@@ -142,6 +291,9 @@ public class FileStorageView extends VBox {
             }
         });
 
+        // contact list
+        contactFileStorageListView = new ContactFileStorageView(client);
+
 
         // sync layout
         HBox syncLayout = new HBox();
@@ -174,6 +326,7 @@ public class FileStorageView extends VBox {
         getChildren().add(createLayout);
         getChildren().add(syncLayout);
         getChildren().add(fileStorageListView);
+        getChildren().add(contactFileStorageListView);
 
         client.getUserData().getStorageDir().addListener(listener);
         update();
