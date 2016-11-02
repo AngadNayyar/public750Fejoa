8aafdbf753f40405cf886de726fb8639906553bf
Clemens Zeidler <clemens.zeidler@gmail.com>
2016-11-03 08:19:42 +1300

    Add some cli options to the server.

--- server/src/main/java/org/fejoa/server/JettyServer.java
+++ server/src/main/java/org/fejoa/server/JettyServer.java
@@ -7,11 +7,15 @@
  */
 package org.fejoa.server;
 
+import org.apache.commons.cli.*;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.server.session.HashSessionIdManager;
 import org.eclipse.jetty.server.session.HashSessionManager;
 import org.eclipse.jetty.server.session.SessionHandler;
+
+import java.io.File;
+import java.net.InetSocketAddress;
 
 
 class DebugSingleton {
@@ -39,16 +43,63 @@ public class JettyServer {
     final Server server;
 
     public static void main(String[] args) throws Exception {
-        JettyServer server = new JettyServer("");
+        Options options = new Options();
+
+        Option input = new Option("h", "host", true, "Host ip address");
+        input.setRequired(true);
+        options.addOption(input);
+
+        Option output = new Option("p", "port", true, "Host port");
+        output.setRequired(true);
+        options.addOption(output);
+
+        Option dirOption = new Option("d", "directory", true, "Storage directory");
+        dirOption.setRequired(false);
+        options.addOption(dirOption);
+
+        CommandLine cmd;
+        try {
+            cmd = new DefaultParser().parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            new HelpFormatter().printHelp("Fejoa Server", options);
+
+            System.exit(1);
+            return;
+        }
+
+        String host = cmd.getOptionValue("host");
+        Integer port;
+        try {
+            port = Integer.parseInt(cmd.getOptionValue("port"));
+        } catch (NumberFormatException e) {
+            System.out.println("Port must be a number");
+            System.exit(1);
+            return;
+        }
+
+        String directory = ".";
+        //if (cmd.hasOption("directory"))
+          //  directory = cmd.getOptionValue("directory");
+
+        JettyServer server = new JettyServer(directory, host, port);
         server.start();
     }
 
     public JettyServer(String baseDir) {
-        this(baseDir, 8080);
+        this(baseDir, null, 8080);
     }
 
     public JettyServer(String baseDir, int port) {
-        server = new Server(port);
+        this(baseDir, null, port);
+    }
+
+    public JettyServer(String baseDir, String host, int port) {
+        System.out.println(new File(baseDir).getAbsolutePath());
+        if (host == null)
+            server = new Server(port);
+        else
+            server = new Server(new InetSocketAddress(host, port));
 
         server.setSessionIdManager(new HashSessionIdManager());
 
