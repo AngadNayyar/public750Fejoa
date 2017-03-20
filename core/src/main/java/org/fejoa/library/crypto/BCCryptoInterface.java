2d7a58be857dcc224261e532e7ad29d46a83d0b4
Clemens Zeidler <clemens.zeidler@gmail.com>
2017-03-21 07:15:10 +1300

    Try to fix the removing of security restrictions.

--- core/src/main/java/org/fejoa/library/crypto/BCCryptoInterface.java
+++ core/src/main/java/org/fejoa/library/crypto/BCCryptoInterface.java
@@ -12,24 +12,75 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.security.*;
 import java.security.spec.ECGenParameterSpec;
 import java.security.spec.KeySpec;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 
 
 public class BCCryptoInterface implements ICryptoInterface {
-    public BCCryptoInterface() {
-        // enable “Unlimited Strength” JCE policy
-        // This is necessary to use AES256!
-        try {
-            Field field = Class.forName("javax.crypto.JceSecurity").getDeclaredField("isRestricted");
-            field.setAccessible(true);
-            field.set(null, java.lang.Boolean.FALSE);
-        } catch (Exception ex) {
+    static class JavaSecurityRestrictionRemover {
+        // Based on http://stackoverflow.com/questions/1179672/how-to-avoid-installing-unlimited-strength-jce-policy-files-when-deploying-an
+        private JavaSecurityRestrictionRemover() {
+            Logger logger = Logger.getGlobal();
+            if (!isRestrictedCryptography()) {
+                logger.fine("Cryptography restrictions removal not needed");
+                return;
+            }
+            try {
+                /*
+                 * Do the following, but with reflection to bypass access checks:
+                 *
+                 * JceSecurity.isRestricted = false;
+                 * JceSecurity.defaultPolicy.perms.clear();
+                 * JceSecurity.defaultPolicy.add(CryptoAllPermission.INSTANCE);
+                 */
+                final Class<?> jceSecurity = Class.forName("javax.crypto.JceSecurity");
+                final Class<?> cryptoPermissions = Class.forName("javax.crypto.CryptoPermissions");
+                final Class<?> cryptoAllPermission = Class.forName("javax.crypto.CryptoAllPermission");
+
+                final Field isRestrictedField = jceSecurity.getDeclaredField("isRestricted");
+                isRestrictedField.setAccessible(true);
+                final Field modifiersField = Field.class.getDeclaredField("modifiers");
+                modifiersField.setAccessible(true);
+                modifiersField.setInt(isRestrictedField, isRestrictedField.getModifiers() & ~Modifier.FINAL);
+                isRestrictedField.set(null, false);
+
+                final Field defaultPolicyField = jceSecurity.getDeclaredField("defaultPolicy");
+                defaultPolicyField.setAccessible(true);
+                final PermissionCollection defaultPolicy = (PermissionCollection) defaultPolicyField.get(null);
+
+                final Field perms = cryptoPermissions.getDeclaredField("perms");
+                perms.setAccessible(true);
+                ((Map<?, ?>) perms.get(defaultPolicy)).clear();
+
+                final Field instance = cryptoAllPermission.getDeclaredField("INSTANCE");
+                instance.setAccessible(true);
+                defaultPolicy.add((Permission) instance.get(null));
+
+                logger.fine("Successfully removed cryptography restrictions");
+            } catch (final Exception e) {
+                logger.log(Level.WARNING, "Failed to remove cryptography restrictions", e);
+            }
         }
+
+        private static boolean isRestrictedCryptography() {
+            // This simply matches the Oracle JRE, but not OpenJDK.
+            return "Java(TM) SE Runtime Environment".equals(System.getProperty("java.runtime.name"));
+        }
+    }
+
+    // enable “Unlimited Strength” JCE policy
+    // This is necessary to use AES256!
+    static private JavaSecurityRestrictionRemover remover = new JavaSecurityRestrictionRemover();
+
+    public BCCryptoInterface() {
         Security.addProvider(new BouncyCastleProvider());
     }
 
